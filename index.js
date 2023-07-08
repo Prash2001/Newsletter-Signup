f45974471320513fa1e471f3059b9aeee4f904f0
Prashant <prashantjapkar11@gmail.com>
2023-07-08 16:26:59 +0530

    Removed debug statements

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
+//importing required modules
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const { subscribe } = require("diagnostics_channel");
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+//bodyparser for converting request params to JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+//All static files are stored in public folder
 app.use(express.static("public/"));
-app.listen(process.env.PORT || 3000,function(){
+//using process.env.port, because if we are hoisting our application on third party server, random port may get selected by thread.
+app.listen(process.env.PORT || 3000, function () {
   console.log("APP is live on port 3000");
 });
-app.get('/', function(req, res){
-res.sendFile(__dirname+"/signup.html");
+//rendering signup form
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
-app.post("/",function(req, res){
+//handling data posted by user
+app.post("/", function (req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const data = {
-    members:[
+    members: [
       {
         email_address: email,
-        status:"subscribed",
+        status: "subscribed",
         merge_fields: {
-          FNAME:firstName,
-          LNAME:lastName
-        }
-      }
-    ]
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
   };
   const jsonData = JSON.stringify(data);
+  //URL to connect to Mail Chimp API
   const url = "https://us10.api.mailchimp.com/3.0/lists/f08b068989";
-  const options ={
-    method : "POST",
-    auth:"prash:"+process.env.SECRET_KEY
-  }
-  const request = https.request(url, options, function(response){
-    
-    response.on("data", function(data){
-      console.log(JSON.parse(data).error_count);
-      //console.log(response);
-      if(response.statusCode ===200 && JSON.parse(data).error_count<=0){
-        res.sendFile(__dirname+"//success.html");
+  // Secret key is part of .env file
+  const options = {
+    method: "POST",
+    auth: "prash:" + process.env.SECRET_KEY,
+  };
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      if (response.statusCode === 200 && JSON.parse(data).error_count <= 0) {
+        res.sendFile(__dirname + "//success.html");
+      } else {
+        res.sendFile(__dirname + "//failure.html");
       }
-      else{
-        res.sendFile(__dirname+"//failure.html");
-      }
-    })
+    });
   });
-request.write(jsonData);
-request.end();
+  request.write(jsonData);
+  request.end();
 });
-app.get("/failure", function(req,res){
+
+//Handling failure case(if user is already signed up then giving him appropriate message)
+app.get("/failure", function (req, res) {
   res.redirect("/");
-})
-//Audience id
-//f08b068989
-//API Key
-//b4e8877fc1f78e0042b52c57d7b86502-us10
+});
